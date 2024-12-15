4fedd6227d868525f6438751b880cfda2efca530
plateahub <wildorapaza95@gmail.com>
2024-12-15 16:07:46 -0500

    camb10

--- django_project/settings.py
+++ django_project/settings.py
@@ -134,3 +134,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ALLOWED_HOSTS = ['localhost','prueba-cusco-production.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS =['http://*','https://prueba-cusco-production.up.railway.app']
