cd1da10d2f4ff90fe4e5d7a4066c8f3852f476fa
plateahub <wildorapaza95@gmail.com>
2024-12-16 23:47:32 -0500

    camb15

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,6 +123,7 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# DONDE
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
